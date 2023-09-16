480579ea263c9f21589f5610d99bb157131329fa
Fabricio Garcia <fabricio.garcia@outlook.com>
2023-09-16 16:56:13 -0600

    Initial commit

---
+++ src/main/java/com/mx/cruddiscografia/CrudDiscografiaApplication.java
@@ -0,0 +1,13 @@
+package com.mx.cruddiscografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDiscografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDiscografiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/cruddiscografia/CrudDiscografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.cruddiscografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDiscografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
